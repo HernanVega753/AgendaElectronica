86919f1d10891546341ea1d3eea1b5df5e7f8332
Hernán Vega <hervega123@gmail.com>
2023-11-14 00:50:44 -0300

    Primer commit

---
+++ AgendaElectronica/src/logica/AgendaElectronica.java
@@ -0,0 +1,12 @@
+
+package logica;
+
+import igu.Pantalla;
+
+public class AgendaElectronica {
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
